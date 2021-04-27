0f259c842945e456c71a7fd46299b0548ed10f6e
iheb <mohamediheb.bchini@esprit.tn>
2021-04-27 01:39:51 +0100

    iheb-new

--- src/main/java/com/application/pidev/Repository/Agency/AgencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.pidev.Repository.Agency;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.pidev.Entity.Agency.Agency;
-
-public interface AgencyRepository extends JpaRepository<Agency, Long>{
-
-}
